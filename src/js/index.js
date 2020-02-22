19a74fd6dd540e93c1393de5c08a75b7add1fb6e
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-02-23 00:11:31 +0100

    added js file for the index page.

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const hire=document.querySelector('#hire');
+const name=document.querySelector('#name');
+const mail=document.querySelector('#mail');
+const term=document.querySelector('#input-term');
+const signUp=document.querySelector('#sign-up');
