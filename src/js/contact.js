9503dd5a2994cfb852c21366630657a872c8bd38
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-02-23 18:32:58 +0100

    Made some changes on css and implemented js validation.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,8 +1,25 @@
 const sendMessage=document.querySelector('.sendBtn');
+const name=document.querySelector('#name');
+const email=document.querySelector('#mail');
+const message=document.querySelector('#message');
+const error=document.querySelector('.error');
 
 
 
 sendMessage.addEventListener('click',(e)=>{
     e.preventDefault();
     
+    const patterns=/^[a-z]+$/i;
+    var mail= /\S+@\S+\.\S+/;
+
+    if(email.value.match(mail)&&name.value.match(patterns)&&message.value!=''){
+        setTimeout(() => {
+            alert('Message Sent');
+        }, 3000);
+    }else{
+        error.innerHTML='Invalid input';
+        error.style.color='red';
+        console.log(name.value);
+    }
+
 })
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,27 @@ const name=document.querySelector('#name');
 const mail=document.querySelector('#mail');
 const term=document.querySelector('#input-term');
 const signUp=document.querySelector('#sign-up');
+const error=document.querySelector('.error');
+const pattern=/\s+/;
+const mailPattern=/\S+@\S+\.\S+/;
+
+signUp.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    if(name.value.match(pattern)||mail.value.match(mailPattern)){
+        if(term.checked){
+            error.innerHTML='';
+            setTimeout(() => {
+                window.location.assign('/thankyou.html');//Navigates the window to thank you page 
+            }, 2000);
+    
+        }else{
+            //$('.error').innerHTML='please accept terms before continuing';
+            error.innerHTML='Please accept the terms befor continuing';
+        }
+       
+    }else{
+        error.innerHTML='Wrong input format.';
+    }
+    
+})
